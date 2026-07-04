7f1a7cfa2df16c3ee1bbc06a062b806beb8122a5
Jorik <jorikbllaca@gmail.com>
2026-07-04 16:48:01 +0200

    h

--- test.py
+++ test.py
@@ -1,9 +1,29 @@
-from gpiozero import Button
+from gpiozero import LED, Button
 from signal import pause
 
-button = Button(21, pull_up=True)
+led1 = LED(17)
+led2 = LED(4)
+led3 = LED(3)
+led4 = LED(2)
 
-button.when_pressed = lambda: print("Pressed!")
-button.when_released = lambda: print("Released!")
+button1 = Button(21, pull_up=True)
+button2 = Button(20, pull_up=True)
+button3 = Button(16, pull_up=True)
+button4 = Button(11, pull_up=True)
+
+button1.when_pressed = lambda: print("Button 1")
+button2.when_pressed = lambda: print("Button 2")
+button3.when_pressed = lambda: print("Button 3")
+button4.when_pressed = lambda: print("Button 4")
+
+print("Press buttons...")
+
+leds = [led1, led2, led3, led4]
+
+def all_off():
+    for led in leds:
+        led.off()
+
+led2.on()
 
 pause()
